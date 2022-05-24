21c253ec2e348f71b5b2d7a7033a1483bcb3053b
aleexpo <101505832+aleexpo@users.noreply.github.com>
2022-05-24 14:12:58 +0300

    uploaded by terraform

--- task-11/static_LB.tf
+++ task-11/static_LB.tf
@@ -1,18 +1,18 @@
-#resource "kubernetes_service" "static-site-loadbalancer" {
-#  metadata {
-#    name = var.service_name
-#  }
-#  spec {
-#    selector = {
-#      app = var.app_name
-#    }
-#    session_affinity = "ClientIP"
-#
-#    port {
-#      port = var.app_port
-#      target_port = var.app_port
-#    }
-#
-#    type = "LoadBalancer"
-#  }
-#}
+resource "kubernetes_service" "static-site-loadbalancer" {
+  metadata {
+    name = var.service_name
+  }
+  spec {
+    selector = {
+      app = var.app_name
+    }
+    session_affinity = "ClientIP"
+
+    port {
+      port = var.app_port
+      target_port = var.app_port
+    }
+
+    type = "LoadBalancer"
+  }
+}
