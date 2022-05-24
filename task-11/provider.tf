bd65ccc1df001d54a3c7187322c2e1535a9e6d98
aleexpo <101505832+aleexpo@users.noreply.github.com>
2022-05-24 12:21:00 +0300

    uploaded by terraform

---
+++ task-11/provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+    github = {
+      source = "integrations/github"
+      version = "~> 4.0"
+    }
+  }
+}
+provider "github" {
+  token = var.token
+}
+
+
+provider "kubernetes" {
+  host = "https://192.168.49.2:8443"
+
+  client_certificate     = file("/home/vagrant/.minikube/profiles/minikube/client.crt")
+  client_key             = file("/home/vagrant/.minikube/profiles/minikube/client.key")
+  cluster_ca_certificate = file("/home/vagrant/.minikube/ca.crt")
+}
